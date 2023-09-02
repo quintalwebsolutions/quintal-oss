c83eef4792752f998210eca1e266dff079d69205
William Ford <william@saphewebdesign.com>
2023-09-02 15:45:21 +0200

    Add eslint configuration

---
+++ packages/config/eslint.js
@@ -0,0 +1,18 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+module.exports = {
+  extends: ['@vercel/style-guide/eslint/node', '@vercel/style-guide/eslint/typescript'],
+  parserOptions: {
+    project,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ['node_modules/', 'dist/', '.turbo/', '.coverage/'],
+};
---
+++ packages/config/vite.js
@@ -0,0 +1,33 @@
+const { resolve } = require('node:path');
+const { defineConfig } = require('vite');
+const dts = require('vite-plugin-dts');
+
+const fileName = {
+  es: `index.mjs`,
+  cjs: `index.cjs`,
+};
+
+const formats = Object.keys(fileName);
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: resolve(process.cwd(), 'src/index.ts'),
+      name: '@quintal/result',
+      formats,
+      fileName: (format) => fileName[format],
+    },
+  },
+  test: {
+    // TODO
+    // benchmark
+    watch: false,
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      reportsDirectory: '.coverage',
+    },
+  },
+  plugins: [dts()],
+});

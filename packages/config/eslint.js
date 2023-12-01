e78f7c9c160033d44cb11b0f09cde1f828dba29c
William Ford <william@saphewebdesign.com>
2023-12-01 11:15:21 +0100

    update docs and some rules

--- packages/config/eslint.js
+++ packages/config/eslint.js
@@ -6,7 +6,11 @@ module.exports = {
   extends: [
     '@vercel/style-guide/eslint/node',
     '@vercel/style-guide/eslint/typescript',
-  ].map(require.resolve),
+  ].map(require.resolve).concat([
+    'next/core-web-vitals',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
+  ]),
   parserOptions: {
     project,
   },
@@ -16,15 +20,22 @@ module.exports = {
     '@typescript-eslint/naming-convention': 'off', // TODO check how to configure this
     '@typescript-eslint/prefer-reduce-type-parameter': 'off', // TODO check if this is justified, reduce is just weird
     '@typescript-eslint/no-confusing-void-expression': 'off',
-    'unicorn/filename-case': [
-      'error',
-      {
-        cases: {
-          camelCase: true,
-          pascalCase: true,
-        },
-      },
-    ],
+    'import/no-default-export': 'off', // TODO how to disable this only for layout, page, etc files
+    'import/no-named-as-default': 'off', // TODO what even is this
+    '@typescript-eslint/no-invalid-void-type': 'off',
+    'unicorn/filename-case': 'off', // TODO how to disable this only for not-found, etc
+    'import/no-extraneous-dependencies': 'off', // TODO how to disable for .prisma/client
+    'no-useless-return': 'off', // TODO test this rule
+    // TODO
+    // 'unicorn/filename-case': [
+    //   'error',
+    //   {
+    //     cases: {
+    //       camelCase: true,
+    //       pascalCase: true,
+    //     },
+    //   },
+    // ],
   },
   settings: {
     'import/resolver': {
@@ -33,5 +44,5 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: ['node_modules/', 'dist/', '.turbo/', '.coverage/'],
+  ignorePatterns: ['node_modules/', 'dist/', '.turbo/', '.coverage/', '.next/'],
 };

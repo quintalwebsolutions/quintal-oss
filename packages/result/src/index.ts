d5bbc4b2cc4252484efc510119c68496416caf66
William <william.ford@riskquest.com>
2024-01-15 16:53:56 +0100

    Add most Rust methods for to Result monad

--- packages/result/src/index.ts
+++ packages/result/src/index.ts
@@ -6,19 +6,167 @@ type IsOk<TOk extends boolean, TTrue, TFalse> = TOk extends true
 
 /** A type that represents either success or failure */
 type ResultConstructor<TOk extends boolean, T, E> = {
-  /** Is `true` if the result is `ok` */
-  isOk: TOk;
-  /** Is `true` if the result is `err` */
-  isErr: IsOk<TOk, false, true>;
   /**
-   * Maps a `Result<T, E>` to `Result<U, E>` by applying a function to a contained `ok` value, leaving an `err` value untouched.
+   * TODO implement these functions
+   * - ok: Converts from `Result<T, E>` to `Option<T>`, discarding the error, if any.
+   * - err: Converts from `Result<T, E>` to `Option<E>`, discarding the success value, if any.
+   * - expect: Returns the contained `ok` value
+   * - expectErr: Returns the contained `err` value
+   * - isOkAnd: Returns `true` if the result is `ok` and the value inside of it matches a predicate.
+   * - isErrAnd: Returns `true` if the result is `err` and the value inside of it matches a predicate.
+   * - flatten & transpose?
    */
-  map: <U>(fn: (val: T) => U) => IsOk<TOk, OkResult<U>, ErrResult<E>>;
+
+  /**
+   * Is `true` if the result is `ok`.
+   *
+   * This property can be used to type-narrow the result value.
+   */
+  isOk: TOk;
+  /**
+   * Is `true` if the result is `err`.
+   *
+   * This property can be used to type-narrow the result value.
+   */
+  isErr: IsOk<TOk, false, true>;
+
+  /**
+   * Calls the provided closure with a reference to the contained value (if `ok`).
+   */
+  inspect: (fn: (value: T) => void) => ResultConstructor<TOk, T, E>;
+  /**
+   * Calls the provided closure with a reference to the contained error (if `err`).
+   */
+  inspectErr: (fn: (error: E) => void) => ResultConstructor<TOk, T, E>;
+
+  /**
+   * Returns `res` if the result is `ok`, otherwise returns its own `err` value.
+   *
+   * * If you are passing the result of a function call to `res`, it is recommended to use `andThen`, which is lazily evaluated.
+   *
+   * @example
+   * const x = ok(2);
+   * const y = err("late error");
+   * expect(x.and(y)).toBe(err("late error"));
+   *
+   * @example
+   * const x = err("early error");
+   * const y = ok("foo");
+   * expect(x.and(y)).toBe(err("early error"));
+   *
+   * @example
+   * const x = err("not a 2");
+   * const y = err("late error");
+   * expect(x.and(y)).toBe(err("not a 2"));
+   *
+   * @example
+   * const x = ok(2);
+   * const y = ok("different result type");
+   * expect(x.and(y)).toBe(ok("different result type"));
+   */
+  and: <U>(res: Result<U, E>) => Result<U, E>;
+  /**
+   * Calls `fn` if the result is `ok`, otherwise return its own `err` value.
+   *
+   * This function can be used for control flow based on Result values.
+   *
+   * @example
+   * const s = (x: number) => x === 42 ? err("bad number") : ok(x * x);
+   * expect(ok(2)).andThen(s)).toBe(ok(4));
+   * expect(ok(42).andThen(s)).toBe(err("bad number"));
+   * expect(err("not a number").andThen(s)).toBe(err("not a number"));
+   */
+  andThen: <U>(fn: (value: T) => Result<U, E>) => Result<U, E>;
+  /**
+   * Returns `res` if the result is `err`, otherwise returns its own `ok` value.
+   *
+   * * If you are passing the result of a function call to `res`, it is recommended to use `orElse`, which is lazily evaluated.
+   *
+   * @example
+   * const x = ok(2);
+   * const y = err("late error");
+   * expect(x.or(y)).toBe(ok(2));
+   *
+   * @example
+   * const x = err("early error");
+   * const y = ok(2);
+   * expect(x.or(y)).toBe(ok(2));
+   *
+   * @example
+   * const x = err("not a 2");
+   * const y = err("late error");
+   * expect(x.or(y)).toBe(err("late error"));
+   *
+   * @example
+   * const x = ok(2);
+   * const y = ok(100);
+   * expect(x.or(y)).toBe(ok(2));
+   */
+  or: <F>(res: Result<T, F>) => Result<T, F>;
+  /**
+   * Calls `fn` if the result is `err`, otherwise returns its own `ok` value.
+   *
+   * This function can be used for control flow based on result values.
+   *
+   * @example
+   * const s = (x: number) => ok(x * x);
+   * const e = (x: number) => err(x);
+   * expect(ok(2).orElse(s).orElse(s)).toBe(ok(2));
+   * expect(ok(2).orElse(e).orElse(s)).toBe(ok(2));
+   * expect(err(3).orElse(s).orElse(e)).toBe(ok(9));
+   * expect(err(3).orElse(e).orElse(e)).toBe(err(3));
+   */
+  orElse: <F>(fn: (error: E) => Result<T, F>) => Result<T, F>;
+
+  /**
+   * Maps a `Result<T, E>` to a `Result<U, E>` by applying a function to a contained `ok` value, leaving an `err` value untouched.
+   *
+   * This function can be used to compose the results of two functions.
+   */
+  map: <U>(fn: (value: T) => U) => IsOk<TOk, OkResult<U>, ErrResult<E>>;
+  /**
+   * Maps a `Result<T, E>` to a `Result<T, F>` by applying a function to a contained `err` value, leaving an `ok` value untouched.
+   *
+   * This function can be used to pass through a successful result while handling an error.
+   */
+  mapErr: <F>(fn: (error: E) => F) => IsOk<TOk, OkResult<T>, ErrResult<F>>;
+  /**
+   * Returns the provided default (if `err`), or applies a function to the contained value (if `ok`).
+   *
+   * This function can be used to unpack a successful result while handling an error.
+   *
+   * * If you are passing the result of a function call to `defaultValue`, it is recommended to use `mapOrElse`, which is lazily evaluated.
+   */
+  mapOr: <U>(defaultValue: U, fn: (value: T) => U) => U;
+  /**
+   * Maps a `Result<T, E>` to `U` by applying fallback function `defaultFn` to an `err` value, or function `fn` to an `ok` value.
+   *
+   * This function can be used to unpack a successful result while handling an error.
+   */
+  mapOrElse: <U>(defaultFn: (error: E) => U, fn: (value: T) => U) => U;
+
   /**
    * Returns the contained `ok` value, or throws the value if it is an `err`.
-   * * Because this function may throw, it is generally discouraged. Instead, prefer to use `unwrapOr`.
+   *
+   * * Because this function may throw, its use is generally discouraged. Instead, prefer to use `ok`, `unwrapOr`, or `unwrapOrElse`.
    */
   unwrap: () => T;
+  /**
+   * Returns the contained `err` value, or throws the value if it is is an `ok`.
+   *
+   * * Because this function may throw, its use is generally discouraged. Instead, prefer to use `err`.
+   */
+  unwrapErr: () => E;
+  /**
+   * Returns the contained `ok` value, or a provided default.
+   *
+   * * If you are passing the result of a function call to `defaultValue`, it is recommended to use `unwrapOrElse`, which is lazily evaluated.
+   */
+  unwrapOr: (defaultValue: T) => T;
+  /**
+   * Returns the contained `ok` value, or computes it from a closure.
+   */
+  unwrapOrElse: (op: (error: E) => T) => T;
 };
 
 export type OkResult<T> = ResultConstructor<true, T, never>;
@@ -29,8 +177,25 @@ export function ok<T>(value: T): OkResult<T> {
   return {
     isOk: true,
     isErr: false,
+    inspect: (fn) => {
+      fn(value);
+      return ok(value);
+    },
+    inspectErr: () => ok(value),
+    and: (res) => res, // TODO test
+    andThen: (fn) => fn(value), // TODO test
+    or: () => ok(value), // TODO test
+    orElse: () => ok(value), // TODO test
     map: (fn) => ok(fn(value)),
+    mapErr: () => ok(value), // TODO test
+    mapOr: (_, fn) => fn(value), // TODO test
+    mapOrElse: (_, fn) => fn(value), // TODO test
     unwrap: () => value,
+    unwrapErr: () => {
+      throw value;
+    },
+    unwrapOr: () => value, // TODO test
+    unwrapOrElse: () => value, // TODO test
   };
 }
 
@@ -38,16 +203,42 @@ export function err<E>(error: E): ErrResult<E> {
   return {
     isOk: false,
     isErr: true,
+    inspect: () => err(error),
+    inspectErr: (fn) => {
+      fn(error);
+      return err(error);
+    },
+    and: () => err(error), // TODO test
+    andThen: () => err(error), // TODO test
+    or: (res) => res, // TODO test
+    orElse: (fn) => fn(error), // TODO test
     map: () => err(error),
+    mapErr: (fn) => err(fn(error)), // TODO test
+    mapOr: (defaultValue) => defaultValue, // TODO test
+    mapOrElse: (defaultFn) => defaultFn(error), // TODO test
     unwrap: () => {
       throw error;
     },
+    unwrapErr: () => error,
+    unwrapOr: (defaultValue) => defaultValue, // TODO test
+    unwrapOrElse: (fn) => fn(error), // TODO test
   };
 }
 
 export function result<T>(fn: () => T): Result<T, unknown> {
   try {
     const value = fn();
+    return ok(value);
+  } catch (e) {
+    return err(e);
+  }
+}
+
+export async function asyncResult<T>(
+  fn: () => Promise<T>,
+): Promise<Result<T, unknown>> {
+  try {
+    const value = (await fn()) as T; // I don't get why this cast is necessary
     return ok(value);
   } catch (e) {
     return err(e);
--- packages/result/test/index.spec.ts
+++ packages/result/test/index.spec.ts
@@ -1,5 +1,5 @@
-import { describe, expect, expectTypeOf, it } from 'vitest';
-import { err, ok, result } from '../src';
+import { describe, expect, expectTypeOf, it, vi } from 'vitest';
+import { asyncResult, err, ok, result } from '../src';
 
 function throwSync(): boolean {
   throw new Error('This is an error');
@@ -18,7 +18,7 @@ async function noThrowAsync(): Promise<boolean> {
 }
 
 describe('Result', () => {
-  it('Allows to create type-safe `ok` and `err` values', () => {
+  it('Is able to create type-safe `ok` and `err` values', () => {
     const okValue = ok('value');
     const errValue = err('error' as const);
     const resultValue = result(noThrowSync);
@@ -34,9 +34,26 @@ describe('Result', () => {
     expect(errValue.isErr).toBe(true);
 
     expectTypeOf(resultValue.isOk).toEqualTypeOf<boolean>();
-    expect(errValue.isOk).toBe(false);
-    expectTypeOf(errValue.isErr).toEqualTypeOf<true>();
-    expect(errValue.isErr).toBe(true);
+    expect(resultValue.isOk).toBe(true);
+    expectTypeOf(resultValue.isErr).toEqualTypeOf<boolean>();
+    expect(resultValue.isErr).toBe(false);
+  });
+
+  it('Allows to type-narrow a generic result type', () => {
+    const unknownResult = result(noThrowSync);
+    expectTypeOf<ReturnType<typeof unknownResult.map>>()
+      .toHaveProperty('isOk')
+      .toEqualTypeOf<boolean>();
+
+    if (unknownResult.isOk) {
+      expectTypeOf(unknownResult.map)
+        .returns.toHaveProperty('isOk')
+        .toEqualTypeOf<true>();
+    } else {
+      expectTypeOf(unknownResult.map)
+        .returns.toHaveProperty('isOk')
+        .toEqualTypeOf<false>();
+    }
   });
 
   it('Allows to unwrap the value in the `Result` monad if present', async () => {
@@ -73,24 +90,74 @@ describe('Result', () => {
     expect(asyncErrResultUnwrap).rejects.toThrow('This is an async error');
   });
 
-  it('Allows to type-narrow a generic result type', () => {
-    const unknownResult = result(noThrowSync);
-    expectTypeOf<ReturnType<typeof unknownResult.map>>()
-      .toHaveProperty('isOk')
-      .toEqualTypeOf<boolean>();
+  it('Allows to unwrap the error in the `Result` monad if present', async () => {
+    const okUnwrapErr = ok(42).unwrapErr;
+    expectTypeOf(okUnwrapErr).returns.toEqualTypeOf<never>();
+    expect(okUnwrapErr).toThrowErrorMatchingInlineSnapshot('42');
 
-    if (unknownResult.isOk) {
-      expectTypeOf(unknownResult.map)
-        .returns.toHaveProperty('isOk')
-        .toEqualTypeOf<true>();
-    } else {
-      expectTypeOf(unknownResult.map)
-        .returns.toHaveProperty('isOk')
-        .toEqualTypeOf<false>();
-    }
+    const errUnwrapErr = err('error' as const).unwrapErr;
+    expectTypeOf(errUnwrapErr).returns.toEqualTypeOf<'error'>();
+    expect(errUnwrapErr()).toBe('error');
+
+    const syncOkResultUnwrapErr = result(noThrowSync).unwrapErr;
+    expectTypeOf<
+      ReturnType<typeof syncOkResultUnwrapErr>
+    >().toEqualTypeOf<unknown>();
+    expect(syncOkResultUnwrapErr).toThrowErrorMatchingInlineSnapshot('true');
+
+    const syncErrResultUnwrapErr = result(throwSync).unwrapErr;
+    expectTypeOf<
+      ReturnType<typeof syncErrResultUnwrapErr>
+    >().toEqualTypeOf<unknown>();
+    expect(syncErrResultUnwrapErr()).toStrictEqual(Error('This is an error'));
+
+    const asyncOkResultUnwrapErr = (await asyncResult(noThrowAsync)).unwrapErr;
+    expectTypeOf<
+      ReturnType<typeof asyncOkResultUnwrapErr>
+    >().toEqualTypeOf<unknown>();
+    expect(asyncOkResultUnwrapErr).toThrowErrorMatchingInlineSnapshot('true');
+
+    const asyncErrResultUnwrapErr = (await asyncResult(throwAsync)).unwrapErr;
+    expectTypeOf<
+      ReturnType<typeof asyncErrResultUnwrapErr>
+    >().toEqualTypeOf<unknown>();
+    expect(asyncErrResultUnwrapErr()).toStrictEqual(
+      Error('This is an async error'),
+    );
   });
 
-  it('Maps a result by applying a functino to a contained ok value, leaving an err value untouched', () => {
+  it('Allows to inspect the value in the result', () => {
+    const mockFn = vi.fn();
+    
+    ok(42).inspect((v) => {
+      expectTypeOf(v).toEqualTypeOf<number>();
+      expect(v).toBe(42);
+      mockFn()
+    });
+    expect(mockFn).toHaveBeenCalledTimes(1)
+    
+    
+    ok(42).inspectErr((v) => {
+      expectTypeOf(v).toEqualTypeOf<never>();
+      mockFn();
+    })
+    expect(mockFn).toHaveBeenCalledTimes(1)
+    
+    err('error' as const).inspect((v) => {
+      expectTypeOf(v).toEqualTypeOf<never>();
+      mockFn();
+    })
+    expect(mockFn).toHaveBeenCalledTimes(1);
+    
+    err('error' as const).inspectErr((v) => {
+      expectTypeOf(v).toEqualTypeOf<'error'>();
+      expect(v).toBe('error');
+      mockFn();
+    })
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  })
+
+  it('Maps a result by applying a function to a contained ok value, leaving an err value untouched', () => {
     const okValue = ok(42).map((v) => {
       expectTypeOf(v).toEqualTypeOf<number>();
       return (v * 2).toString();
@@ -112,24 +179,32 @@ describe('Result', () => {
     expect(okValue.isOk).toBe(true);
     expectTypeOf(okValue.isErr).toEqualTypeOf<false>();
     expect(okValue.isErr).toBe(false);
+    expectTypeOf(okValue.unwrap).returns.toEqualTypeOf<string>();
     expect(okValue.unwrap()).toBe('84');
 
     expectTypeOf(errValue.isOk).toEqualTypeOf<false>();
     expect(errValue.isOk).toBe(false);
     expectTypeOf(errValue.isErr).toEqualTypeOf<true>();
     expect(errValue.isErr).toBe(true);
-    expect(errValue.unwrap).toThrow('error');
+    expectTypeOf(errValue.unwrap).returns.toEqualTypeOf<never>();
+    expect(errValue.unwrapErr()).toBe('error');
 
     expectTypeOf(okResultValue.isOk).toEqualTypeOf<boolean>();
     expect(okResultValue.isOk).toBe(true);
     expectTypeOf(okResultValue.isErr).toEqualTypeOf<boolean>();
     expect(okResultValue.isErr).toBe(false);
+    expectTypeOf<
+      ReturnType<typeof okResultValue.unwrap>
+    >().toEqualTypeOf<boolean>();
     expect(okResultValue.unwrap()).toBe(false);
 
     expectTypeOf(errResultValue.isOk).toEqualTypeOf<boolean>();
     expect(errResultValue.isOk).toBe(false);
     expectTypeOf(errResultValue.isErr).toEqualTypeOf<boolean>();
     expect(errResultValue.isErr).toBe(true);
-    expect(errResultValue.unwrap).toThrow('This is an error');
+    expectTypeOf<
+      ReturnType<typeof errResultValue.unwrap>
+    >().toEqualTypeOf<boolean>();
+    expect(errResultValue.unwrapErr()).toStrictEqual(Error('This is an error'));
   });
 });

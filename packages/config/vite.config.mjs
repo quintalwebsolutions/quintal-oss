1c8353bc919c1b28a97a84fb7d388854fcad5c82
William <william@quintalwebsolutions.com>
2024-09-03 12:34:40 +0200

    Rollback config changes

--- packages/config/vite.config.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * THIS FILE IS (PARTIALLY) AUTO-GENERATED USING `pnpm generate`.
- * TO EDIT THE CONTENT, PLEASE MODIFY `/workspace.ts` OR `/scripts/generate.ts`
- */
-
-import getRecommended from '@quintal/config/vite';
-
-export default getRecommended('@quintal/config');
